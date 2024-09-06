7f9d00f9d578db8da45aef05cfadb1219d69535b
Orlando <orlandmejia374@gmail.com>
2024-09-06 17:55:53 -0600

    Template, login, 401, 404, 500

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+// la ruta raiz nos está devolviendo la vista welcome
 Route::get('/', function () {
-    return view('welcome');
+    return view('template');
+});
+
+// ruta para el login
+Route::get('/login', function () {
+    // retorna lo que está dentro de la carpeta auth/login.blade.php
+    return view('auth.login');
+});
+
+// rutas para los errores 401, 404 y 500
+Route::get('/401', function () {
+    // retorna lo que está dentro de la carpeta 
+    return view('pages.401');
+});
+
+Route::get('/404', function () {
+    // retorna lo que está dentro de la carpeta 
+    return view('pages.404');
+});
+
+Route::get('/500', function () {
+    // retorna lo que está dentro de la carpeta 
+    return view('pages.500');
 });
